a24c5e3017d17d827104b3f8d8e51add2434d7fd
zgyunfan <zgyunfan>
2019-04-04 00:16:18 +0800

    完成关机和音量控制

--- src/renderer/js/system.ts
+++ src/renderer/js/system.ts
@@ -1,4 +1,5 @@
 import * as child_process from "child_process";
+import * as loudness from "loudness";
 
 /**
  * 关机
@@ -6,4 +7,24 @@ import * as child_process from "child_process";
  */
 export function shutdown(wait: number = 10): child_process.ChildProcess {
     return child_process.exec(`shutdown -s -t ${wait}`);
+}
+
+/** 获取当前是否静音 */
+export function auidoGetMuted(): Promise<boolean> {
+    return loudness.getMuted();
+}
+
+/** 获取当前音量值 */
+export function auidoGetVolume(): Promise<number> {
+    return loudness.getVolume();
+}
+
+/** 设置是否静音 */
+export function auidoSetMuted(muted: boolean): Promise<void> {
+    return loudness.setMuted(muted);
+}
+
+/** 设置音量值 */
+export function auidoSetVolume(volume: number): Promise<void> {
+    return loudness.setVolume(volume);
 }
